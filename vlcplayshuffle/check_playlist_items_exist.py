cc69bafb0423cd01837ba65b5aab82619e5c3d0b
Xpl0itU <24777100+Xpl0itU@users.noreply.github.com>
2023-11-28 13:59:36 +0100

    docs(check_playlist_items_exist): Add documentation to the check_playlist_items_exist function

--- vlcplayshuffle/check_playlist_items_exist.py
+++ vlcplayshuffle/check_playlist_items_exist.py
@@ -3,6 +3,15 @@ import os
 
 
 def check_playlist_items_exist(track_paths: Iterable[str]) -> Dict[str, bool]:
+    """
+    Checks if the paths passed as argument exist.
+
+    Args:
+        track_paths (Iterable[str]): An iterable of paths to check their existence.
+    
+    Returns:
+        Dict[str, bool]: A dict with the path as a key and if it exists as a value.
+    """
     final_dict = {}
     for track_path in track_paths:
         final_dict[track_path] = os.path.exists(track_path)
